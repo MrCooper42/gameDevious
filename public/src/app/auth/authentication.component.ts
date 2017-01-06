358d91395f4ea4cf82e2135f9f1b1d06f8daa984
MrCooper42 <mr.cooper42co@gmail.com>
2017-01-06 00:38:35 -0700

    contact info added

--- public/src/app/auth/authentication.component.ts
+++ public/src/app/auth/authentication.component.ts
@@ -11,17 +11,34 @@ import { AuthService } from './auth.service';
           <li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
           <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['signin']">Signin</a></li>
           <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['logout']">Logout</a></li>
+          <li routerLinkActive="active" (click)="contact()"><a [routerLink]="['logout']">Contact</a></li>
         </ul>
       </nav>
     </header>
     <div class="row spacing">
       <router-outlet></router-outlet>
     </div>
+    <hr>
+    <br>
+    <div class="containter text-center" *ngIf="showContact">
+    <h1>Matthew Cooper</h1>
+    <h3>email: mr.cooper42co@gmail.com</h3>
+    <h3>github: https://github.com/MrCooper42</h3>
+    <h3>linkedin: https://www.linkedin.com/in/mrcooper42</h3>
+    </div>
+    <hr>
   `
 })
 
 export class AuthenticationComponent {
+
+    showContact: Boolean = false;
+
     constructor(private authService: AuthService) { }
+
+    contact() {
+      this.showContact = !this.showContact;
+    }
 
     isLoggedIn() {
         return this.authService.isLoggedIn();
